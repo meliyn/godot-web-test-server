fca1109de3cd746719292a98402358edc0c02fc5
adenpun <35649969+aappaapp@users.noreply.github.com>
2023-05-26 03:29:20 +0800

    Initial Commit

---
+++ main.ts
@@ -0,0 +1,37 @@
+import { program } from "npm:commander@10";
+import { Application } from "https://deno.land/x/oak@v12.5.0/mod.ts";
+
+interface Options {
+    baseUrl: string;
+    index: string;
+    port: number;
+}
+
+export async function serve(options: Options) {
+    const app = new Application();
+
+    app.use(async (ctx) => {
+        await ctx.send({
+            root: options.baseUrl,
+            index: options.index,
+        });
+    });
+
+    await app.listen({ port: options.port });
+}
+
+if (import.meta.main) {
+    program
+        .command("serve")
+        .option("-d, --baseDir <dir>", "Base Directory", Deno.cwd())
+        .option("-i, --index <name>", "Index", "index.html")
+        .option("-p, --port <port>", "Port", "8080")
+        .action((options) => {
+            serve({
+                baseUrl: options.baseDir,
+                index: options.index,
+                port: parseInt(options.port),
+            });
+        });
+    program.parse();
+}
